6f60a509ec6e0c716df1c39f19f99ccefd230a89
christianpatrick2005 <144037125+christianpatrick2005@users.noreply.github.com>
2023-09-13 01:28:55 +0700

    first commit

---
+++ js/tugas.js
@@ -0,0 +1,14 @@
+
+function Hitung1(){
+   let pertama = document.getElementById("InputField").value;
+       let kedua = document.getElementById("InputField").value;
+       let hasil = parseInt(pertama) * parseInt(kedua);
+       let inputElement = document.getElementById('InputField');
+       inputElement.value = ("Hasilnya ialah :", hasil)
+}
+function Hitung2(){
+    let pertama = document.getElementById("InputField2").value;
+        let kedua = document.getElementById("InputField2").value;
+        let hasil = 4 * parseInt(kedua);
+        let inputElement = document.getElementById('InputField2');
+        inputElement.value = ("Hasilnya ialah :", hasil)}
